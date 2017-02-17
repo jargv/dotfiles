5c3d3e7033cd9133015ef304a10d07aa36b862ff
Jonathan Geddes <geddes.jonathan@gmail.com>
2017-02-16 23:26:17 -0700

    Even more hyper config

--- dotfiles/hyperterm.js
+++ dotfiles/hyperterm.js
@@ -1,15 +1,18 @@
 module.exports = {
   config: {
+    copyOnSelect: true,
     fontSize: 14,
     modifierKeys: {
       altIsMeta: true,
     },
-    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"DejaVu Sans Mono", "Lucida Console", monospace',
     borderColor: '#f33',
     bell: false,
     css: '', // custom css to embed in the main window
     termCSS: '', // custom css to embed in the terminal window
     padding: '0 0', // custom padding (css format, i.e.: `top right bottom left`)
+    showHamburgerMenu: true,
+    showWindowControls: true,
   },
   plugins: ['hyperterm-oceanic-next'],
   localPlugins: [] //plugins in development
