3f962af1dd263390f8f1522eae050b360b9f6e50
Jonathan Geddes <geddes.jonathan@gmail.com>
2022-09-24 14:12:47 -0600

    Add ranger config

--- install.sh
+++ install.sh
@@ -14,8 +14,13 @@ done;
 # vim directory setup
 # ln -snf $dir/vim $HOME/.vim
 
+DOT_CONFIG=${XDG_CONFIG_HOME:-~/.config}
+
 # nvim directory setup
-ln -snf $dir/nvim ${XDG_CONFIG_HOME:-~/.config}/nvim
+ln -snf $dir/nvim ${DOT_CONFIG}/nvim
+
+# ranger config setup
+ln -snf $dir/ranger ${DOT_CONFIG}/ranger
 
 echo config installed
 echo "don't forget tpm ~/config/setup/tpm.sh"
