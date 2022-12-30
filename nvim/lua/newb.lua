a490a1007f6612e889a1fe3f058845c91ea56c37
Jonathan Geddes <geddes.jonathan@gmail.com>
2022-12-30 12:18:01 -0700

    Make newb commands not rely on vim's cwd

--- nvim/lua/newb.lua
+++ nvim/lua/newb.lua
@@ -1,20 +1,31 @@
 --[[ TODOS:
-- look into making this work on all new buffers instead of requiring a fn
+- look into making this work on all new buffers instead of requiring an explicit fn
+- look into collecting a terminal's cwd and using it
 ]]
 
 local newb = {}
 
+local function updir(dir, chdir)
+  local newdir = vim.fn.fnamemodify(dir, ":h")
+  print(newdir)
+  chdir(newdir)
+end
+
 local new_buffer_options = {
-  {key=".", cmd=":e term:///bin/zsh<cr>",     desc="start a terminal"},
-  {key="d", cmd=":Explore<cr>",               desc="open a directory"},
-  {key="o", cmd=":FZF --inline-info<cr>",     desc="search for a file"},
-  {key="i", cmd=":Buffers<cr>",               desc="search buffers by name"},
-  {key="h", cmd=":e term://tig<cr>",          desc="git history (tig)"},
-  {key="q", cmd=":q!<cr>",                    desc="quit"},
+  {key=".", cmd = ":e term://$dir///bin/zsh", desc="start a terminal"},
+  {key="d", cmd = ":Explore $dir",           desc="open a directory"},
+  {key="o", cmd = ":FZF --inline-info $dir", desc="search for a file"},
+  {key="i", cmd = ":Buffers",           desc="search buffers by name"},
+  {key="h", cmd = ":e term://$dir//tig", desc="git history (tig)"},
+  {key="u", cmd = updir              ,  desc="cd .."},
+  {key="q", cmd = ":q!",                desc="quit"},
 }
 
 function newb.create(split_command)
   return function()
+    -- the directory starts with buffer *before* the split command
+    local dir = vim.fn.expand("%:p:h")
+
     if split_command ~= nil then
       vim.cmd(split_command)
     end
@@ -24,22 +35,59 @@ function newb.create(split_command)
     vim.cmd("b "..buf)
     vim.bo.bufhidden = "wipe"
 
-    -- set up the key bindings and collect the instructions
     local instructions = {}
-    for _,val in ipairs(new_buffer_options) do
-      vim.api.nvim_buf_set_keymap(buf, "n", val.key, val.cmd, {nowait=true})
-      table.insert(instructions, val.key.." -> "..val.desc)
+
+    local function chdir(newdir)
+      dir = newdir
     end
 
-    -- set the contents to the instructions
-    vim.api.nvim_buf_set_lines(buf, -1, -1, false, instructions)
+    local function render()
+      local lines = {dir}
+      for _, val in pairs(instructions) do
+        table.insert(lines, val)
+      end
+      -- set the contents to the instructions
+      vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    end
+
+    local function setup()
+      -- set up the key bindings and collect the instructions
+      for _,val in ipairs(new_buffer_options) do
+        if type(val.cmd) == 'function' then
+          vim.api.nvim_buf_set_keymap(buf, "n", val.key, "",  {
+            nowait=true,
+            callback=function()
+              local dir_before = dir
+              val.cmd(dir, chdir)
+              if dir_before ~= dir then
+                render()
+              end
+            end
+          })
+        else
+          vim.api.nvim_buf_set_keymap(buf, "n", val.key, "", {
+            nowait=true,
+            callback = function()
+              local cmd = string.gsub(val.cmd, "$dir", dir)
+              print(cmd)
+              vim.cmd(cmd)
+            end
+          })
+        end
+        table.insert(instructions, val.key.." -> "..val.desc)
+      end
+    end
+
+    setup()
+    render()
 
     -- clean up the old buffer, unless it was a named file
-    if
-      vim.fn.bufname(tonumber(starting_buf)) == "" and
-      vim.api.nvim_buf_is_loaded(starting_buf) then
+    if vim.fn.bufname(tonumber(starting_buf)) == ""
+    and vim.api.nvim_buf_is_loaded(starting_buf)
+    then
       vim.cmd("silent bwipe! "..starting_buf)
     end
   end
 end
+
 return newb
