0a5bc7d721c2abed2949026df778551e9192f9dd
Jonathan Geddes <geddes.jonathan@gmail.com>
2023-01-02 15:40:24 -0700

    Move plugin setup into init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,338 @@
---[[ stuff to look into:
+--[[
+
+stuff to look into:
  - null-ls
  - treesitter text objects
  - telescope to replace fzf
+
 TODO:
  - fix up the smart enter key
+
 ]]
+
 -- setup {{{1
 vim.g.mapleader = ' '
 --vim.opt.shortmess:append({I = true}) -- don't do intro message at startup
 -- local isLinux = vim.fn.system('uname') == "Linux\n"
 
--- run plugin setup (TODO: look into packer.nvim ) {{{1
-vim.cmd [[source ~/config/nvim/plugins.vim]]
+local mapping = require("mapping")
+
+local leader = mapping.withPrefix("<leader>")
+local normal = mapping()
+local visual = mapping.inMode("v")
+local terminal = mapping.inMode("t")
+
+-- plugins {{{1
+-- bootstrap setup {{{2
+local plugDir = vim.fn.expand "~/config/nvim/plug"
+local function checkPluginSetup()
+  local plugUrl = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  local plugFile = plugDir .. "/plug/autoload/plug.vim"
+
+  -- already installed?
+  if 1 == vim.fn.isdirectory(plugDir) then
+    print "initial setup already complete"
+    return false
+  end
+
+  print "performing initial plugin setup..."
+
+  -- bootstrap by downloading
+  local result = vim.fn.system {
+    "curl", "-fLo", plugFile, "--create-dirs", plugUrl
+  }
+  local error = vim.api.nvim_get_vvar("shell_error")
+  if error ~= 0 or #result == 0 then
+    print "error downloading plug, no plugins will be installed"
+    return false
+  end
+
+  return true
+end
+
+-- regular setup {{{2
+vim.cmd [[filetype off]]
+local initial_plugin_setup_is_required = checkPluginSetup()
+vim.opt.runtimepath:append(plugDir.."/plug")
+vim.call("plug#begin", '~/config/nvim/plug')
+local plugin_setup_funcs = {}
+local Plug = vim.fn['plug#']
+
+-- Language-specifig plugins {{{2
+-- rust {{{3
+-- Plug 'rust-lang/rust.vim'
+-- Plug 'racer-rust/vim-racer'
+vim.g.racer_experimental_completer = 1
+vim.g.ycm_rust_src_path = '~/.multirust/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src'
+
+-- go {{{3
+-- Plug 'jargv/vim-go-error-folds'
+-- Plug 'fatih/vim-go'
+vim.g.go_fmt_command = "goimports"
+vim.g.go_fmt_command = "gofmt"
+vim.g.go_fmt_fail_silently = 1
+vim.g.go_def_mapping_enabled = 0
+
+--don't do whitespace errors, go fmt will eliminate them
+vim.g.go_highlight_trailing_whitespace_error = 0
+vim.g.go_highlight_array_whitespace_error = 0
+vim.g.go_highlight_chan_whitespace_error = 0
+vim.g.go_highlight_space_tab_error = 0
+
+--highlight a bunch of stuff
+vim.g.go_highlight_operators = 1
+vim.g.go_highlight_functions = 1
+vim.g.go_highlight_methods = 1
+vim.g.go_highlight_structs = 1
+vim.g.go_highlight_interfaces = 1
+vim.g.go_highlight_build_constraints = 1
+vim.g.go_auto_type_info = 0
+
+--I like my own K key, thanks
+vim.g.go_doc_keywordprg_enabled = 0
+
+--I only want my own snippets
+vim.g.go_snippet_engine = ""
+
+--I don't want the templates
+vim.g.go_template_autocreate = 0
+
+-- "Scope guru to the whole gopath
+-- vim.g.go_guru_scope = [""]
+
+
+-- javascript/typescript {{{3
+-- Plug 'ternjs/tern_for_vim'
+-- Plug 'jxnblk/vim-mdx-js'
+Plug 'pangloss/vim-javascript'
+-- Plug 'mxw/vim-jsx'
+-- Plug 'jelera/vim-javascript-syntax'
+vim.g.jsx_ext_required = 0
+Plug 'leafgarland/typescript-vim'
+-- Plug 'peitalin/vim-jsx-typescript'
+-- Plug 'MaxMellon/vim-jsx-pretty'
+-- html {{{3
+Plug 'othree/html5.vim'
+
+-- purescript {{{3
+-- Plug 'raichoo/purescript-vim'
+
+-- css {{{3
+Plug 'JulesWang/css.vim'
+
+-- html {{{3
+-- Plug 'mattn/emmet-vim'
+-- toml {{{3
+Plug 'cespare/vim-toml'
+-- docker {{{3
+-- Plug 'ekalinin/Dockerfile.vim'
+
+-- glsl {{{3
+Plug 'vim-scripts/glsl.vim'
+
+-- lua {{{3
+-- Plug 'SpaceVim/vim-swig'
+
+-- zig {{{3
+Plug 'ziglang/zig.vim'
+vim.g.zig_fmt_autosave = 0
+
+-- colorschemes {{{2
+-- Plug 'flazz/vim-colorschemes'
+Plug 'arcticicestudio/nord-vim'
+Plug 'sainnhe/everforest'
+Plug 'trevordmiller/nova-vim'
+Plug 'AlessandroYorba/Arcadia'
+Plug 'jnurmine/Zenburn'
+Plug('sonph/onehalf', { rtp = 'vim' })
+-- }}}
+
+Plug 'nvim-lua/plenary.nvim'
+
+Plug 'freitass/todo.txt-vim'
+Plug 'junegunn/limelight.vim'
+Plug 'junegunn/goyo.vim'
+Plug 'Raimondi/delimitMate'
+Plug 'PeterRincker/vim-argumentative'
+Plug 'reedes/vim-pencil'
+Plug 'tpope/vim-obsession'
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-commentary'
+Plug 'tpope/vim-sleuth'
+Plug 'will133/vim-dirdiff'
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'WhoIsSethDaniel/toggle-lsp-diagnostics.nvim'
+
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/nvim-cmp'
+
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+-- Plug 'williamboman/mason.nvim' {{{2
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+table.insert(plugin_setup_funcs, function()
+  require"mason".setup()
+  require"mason-lspconfig".setup()
+end)
+
+-- Plug 'SmiteshP/nvim-navic' {{{2
+Plug 'SmiteshP/nvim-navic'
+vim.g.winbarShown = 0
+leader.v = function()
+  vim.g.winbarShown = not vim.g.winbarShown
+  if vim.g.winbarShown then
+    vim.opt.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+  else
+    vim.opt.winbar = ""
+  end
+end
+table.insert(plugin_setup_funcs, function()
+  require"nvim-navic".setup {
+    icons = {
+      File          = "",
+      Module        = "",
+      Namespace     = "",
+      Package       = "",
+      Class         = "",
+      Method        = "",
+      Property      = "",
+      Field         = "",
+      Constructor   = "",
+      Enum          = "",
+      Interface     = "",
+      Function      = "",
+      Variable      = "",
+      Constant      = "",
+      String        = "",
+      Number        = "",
+      Boolean       = "",
+      Array         = "",
+      Object        = "",
+      Key           = "",
+      Null          = "",
+      EnumMember    = "",
+      Struct        = "",
+      Event         = "",
+      Operator      = "",
+      TypeParameter = "",
+      },
+    highlight = false,
+    separator = " ➤ ",
+    depth_limit = 0,
+    depth_limit_indicator = "..",
+  }
+end)
+
+-- Plug 'mazubieta/gitlink-vim' {{{2
+Plug 'mazubieta/gitlink-vim'
+vim.cmd [[
+  function! CopyGitLink(...) range
+    redir @+
+    echo gitlink#GitLink(get(a:, 1, 0))
+    redir END
+  endfunction
+  nmap <leader>gl :call CopyGitLink()<CR>
+  vmap <leader>gl :call CopyGitLink(1)<CR>
+]]
+
+
+-- Plug 'dense-analysis/ale' {{{2
+vim.g.ale_fixers = {
+  javascript = {'prettier'},
+  typescript = {'prettier'},
+  javascriptreact = {'prettier'},
+  css  = {'prettier'},
+  go  = {'goimports'},
+  -- c = {'clang-format', 'clangtidy'},
+}
+
+vim.g.ale_linters_explicit = 1
+vim.g.ale_fix_on_save = 1
+vim.g.ale_completion_autoimport = 1
+Plug 'dense-analysis/ale'
+
+-- Plug 'junegunn/fzf' {{{2
+vim.g.fzf_buffers_jump = 1
+Plug 'junegunn/fzf'
+Plug 'junegunn/fzf.vim'
+
+-- Plug 'tpope/vim-vinegar' {{{2
+Plug 'tpope/vim-vinegar'
+
+-- Plug  'SirVer/UltiSnips' {{{2
+Plug 'SirVer/UltiSnips'
+vim.g.UltiSnipsExpandTrigger="<tab>"
+vim.g.UltiSnipsJumpForwardTrigger="<tab>"
+vim.g.UltiSnipsJumpBackwardTrigger="<s-tab>"
+
+vim.g.UltiSnipsEditSplit = 'vertical'
+vim.g.UltiSnipsSnippetsDir = '~/config/nvim/my_snippets'
+
+vim.g.UltiSnipsSnippetDirectories = {"my_snippets"}
+leader.s = ":UltiSnipsEdit<CR>"
+
+-- Plug 'phaazon/hop.nvim' {{{2
+Plug 'phaazon/hop.nvim'
+leader.f = ":HopChar1<cr>"
+leader.F = ":HopChar1MW<cr>"
+table.insert(plugin_setup_funcs, function()
+  require('hop').setup()
+end)
+
+-- Plug 'lewis6991/gitsigns.nvim' {{{2
+Plug 'lewis6991/gitsigns.nvim'
+vim.cmd [[
+  func! GitAdjacentChange(next)
+    if &diff
+      if a:next
+        normal ]c
+        normal zz
+      else
+        normal [c
+        normal zz
+      endif
+    else
+      if a:next
+        lua package.loaded.gitsigns.next_hunk()
+        normal zz
+      else
+        lua package.loaded.gitsigns.prev_hunk()
+        normal zz
+      endif
+    endif
+  endfunc
+  nnoremap gn :call GitAdjacentChange(1)<cr>
+  nnoremap gp :call GitAdjacentChange(0)<cr>
+  nnoremap gr :Gitsigns reset_hunk<cr>
+  nnoremap gs :Gitsigns stage_hunk<cr>
+  nnoremap gS :Gitsigns undo_stage_hunk<cr>
+  onoremap ih :<C-U>Gitsigns select_hunk<cr>
+  xnoremap ih :<C-U>Gitsigns select_hunk<cr>
+]]
+table.insert(plugin_setup_funcs, function()
+  require('gitsigns').setup()
+end)
+--}}}
+
+-- finalize plugin setup {{{2
+vim.call("plug#end")
+-- my config should override EVERYTHING
+vim.opt.runtimepath:remove "~/config/nvim" --remove first, added to the end
+vim.opt.runtimepath:append "~/config/nvim"
+
+if initial_plugin_setup_is_required then
+  vim.cmd [[ PlugInstall! ]]
+end
+
+-- run the config funcs
+for _,fn in ipairs(plugin_setup_funcs) do
+  fn()
+end
+-- }}}
 
 -- color settings {{{1
 vim.opt.termguicolors = true
@@ -26,14 +347,6 @@ vim.cmd [[ colorscheme everforest | set bg=dark ]]
 
 --:NoMatchParen
 vim.g.loaded_matchparen = 1
-
--- set up key mapping objects  {{{1
-local mapping = require("mapping")
-
-local leader = mapping.withPrefix("<leader>")
-local normal = mapping()
-local visual = mapping.inMode("v")
-local terminal = mapping.inMode("t")
 
 -- prototype settings {{{1
 visual.v = "`[o`]"
@@ -375,8 +688,7 @@ end
 -- reload config
 local function reloadConfig()
   local ft_parts = vim.fn.split(vim.bo.filetype, "\\.")
-  vim.cmd "tabe ~/config/nvim/plugins.vim"
-  vim.cmd "vsplit ~/config/nvim/init.lua"
+  vim.cmd "tabe ~/config/nvim/init.lua"
   if #ft_parts ~= 0 then
     vim.cmd("vsplit ~/config/nvim/ftplugin/"..ft_parts[1]..".vim")
   end
