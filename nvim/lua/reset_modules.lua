5af35db1e9738c7f73ccc291a09be56d8e4b0f3a
Jonathan Geddes <geddes.jonathan@gmail.com>
2023-01-05 16:05:49 -0700

    Less overkill with reset_modules code...

--- nvim/lua/reset_modules.lua
+++ nvim/lua/reset_modules.lua
@@ -1,12 +1,18 @@
--- reset all loaded modules so everything will be reloaded. Overkill?
+-- reset my modules only, so they are reloaded propery
 return function()
-  local package_names = {}
-  for name in pairs(package.loaded) do
-    table.insert(package_names, name)
+  -- collect the output of local packages
+  local module_dir = "ls ~/config/nvim/lua"
+  local module_dir_ls = vim.fn.system(module_dir)
+  if 0 ~= vim.api.nvim_get_vvar("shell_error") then
+    print "error listing module directory"
+    return
   end
 
-  for _,name in ipairs(package_names) do
-    package.loaded[name] = nil
+  -- "unload" each package to ensure it's reloaded on next require
+  for file in module_dir_ls:gmatch("[^\r\n]+") do
+    local pkg_name = file:match("^(.*).lua")
+    print(file)
+    print(pkg_name)
+    package.loaded[pkg_name] = nil
   end
 end
-
