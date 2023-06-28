7a4a0419af3a478388cf717c019a004d086d56a1
Jonathan Geddes <geddes.jonathan@gmail.com>
2023-06-27 20:30:18 -0600

    Install also handles config dirs

--- install.sh
+++ install.sh
@@ -1,28 +1,25 @@
 #! /usr/bin/env bash
+set -euo pipefail
+set -v
+set -o xtrace
 
-#install all the dotfiles
+DOT_CONFIG=${XDG_CONFIG_HOME:-~/.config}
+
+# install all the dotfiles
 dir=$(pwd)
-for dotfile in dotfiles/*; do
+for dotfile in dotfiles/* ; do
   dest=$(echo $dotfile | sed "
     s/^dotfiles\///
     s/\.el$//
     s/\.vim$//
   ")
-  ln -snf $dir/$dotfile $HOME/.$dest
+
+  if [[ -d $dotfile ]]; then
+    ln -snf $dir/$dotfile $DOT_CONFIG/$dest
+  else
+    ln -snf $dir/$dotfile $HOME/.$dest
+  fi
 done;
-
-# vim directory setup
-# ln -snf $dir/vim $HOME/.vim
-
-DOT_CONFIG=${XDG_CONFIG_HOME:-~/.config}
-
-# nvim directory setup
-ln -snf $dir/nvim ${DOT_CONFIG}/nvim
-
-# ranger config setup
-ln -snf $dir/ranger ${DOT_CONFIG}/ranger
-ln -snf $dir/alacritty ${DOT_CONFIG}/alacritty
-ln -snf $dir/kitty ${DOT_CONFIG}/kitty
 
 echo config installed
 echo "don't forget tpm ~/config/setup/tpm.sh"
