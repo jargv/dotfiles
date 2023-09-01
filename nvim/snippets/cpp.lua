8a1fba6d00e201cbf68da8f060a1fc3085b2310e
Jonathan Geddes <geddes.jonathan@gmail.com>
2023-09-01 15:39:34 -0600

    Some c++ snippets

--- nvim/snippets/cpp.lua
+++ nvim/snippets/cpp.lua
@@ -1,12 +1,12 @@
 return {
-  -- if
+  -- if {{{1
   s("if", fmta([[
     if (<>) {
       <>
     }
   ]], {i(1, "true"), i(0)})),
 
-  -- For
+  -- For {{{1
   s("for", fmta([[
     for(<>){
       <>
@@ -18,5 +18,46 @@ return {
       fmt("auto {} : {}", {i(2, "item"), i(1, "items")}),
     }),
     i(0)
-  }))
+  })),
+
+  -- fn {{{1
+  s('fn', fmta([[
+    auto <> = [<>](<>){
+      <>
+    };
+  ]], {
+    i(1),
+    i(3),
+    i(2),
+    i(0),
+  })),
+
+  -- lm {{{1
+  s('lm', fmta([[
+    [<>](<>){
+      <>
+    }
+  ]], {
+    i(2),
+    i(1),
+    i(0),
+  })),
+
+  -- l {{{1
+  s('l', fmt([[
+    std::cout << "{}" << {} << std::endl;
+  ]], {
+    i(1),
+    f(function (args)
+      local parts = vim.fn.split(args[1][1], "\\,")
+      local sep = ""
+      local res = ""
+      for _, val in ipairs(parts) do
+        res = res .. sep .. val
+        sep = " << "
+      end
+      return res
+    end, {1})
+  })),
+
 }
