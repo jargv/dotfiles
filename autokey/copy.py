21217a72b85319a44fdf7a8170bcab04b9682826
Jonathan Geddes <geddes.jonathan@gmail.com>
2026-01-15 22:15:37 -0700

    Remove old config for autokey, hyperterm, and a bunch of unused scripts

--- autokey/copy.py
+++
@@ -1,4 +0,0 @@
-if window.get_active_title() == "Terminal":
-    keyboard.send_keys("<super>+c")
-else:
-    keyboard.send_keys("<ctrl>+c")
--- autokey/undo.py
+++
@@ -1,4 +0,0 @@
-if window.get_active_title() == "Terminal":
-    keyboard.send_keys("<super>+z")
-else:
-    keyboard.send_keys("<ctrl>+z")
